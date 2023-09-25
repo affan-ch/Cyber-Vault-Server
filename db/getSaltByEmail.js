e8bd031132092a7c644f18550b6e3c90ac71ba68
muhammadaffanmaqsood <muhammadaffanmaqsood@gmail.com>
2023-09-25 18:12:38 +0500

    Add Login Functionality

---
+++ db/getSaltByEmail.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+async function getSaltByEmail(link, databaseName, collectionName, object) {
+    const client = new MongoClient(link);
+    
+    try {
+        await client.connect();
+        
+        const db = client.db(databaseName);
+        const collection = db.collection(collectionName);
+
+        // Check if a record with the provided email and password exists
+        const result = await collection.findOne({ email: object.email});
+
+        if (result) {
+            // If a matching record is found, return the name and email
+            return { salt: result.salt };
+        } else {
+            // If no matching record is found, return null or an error message
+            return null;
+        }
+    } finally {
+        // Ensure that the client is always closed, even if an error occurs
+        client.close();
+    }
+}
+
+export default getSaltByEmail;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import dbConfig from "./db/connection.js";
 import signup from "./db/signup.js";
+import login from "./db/login.js";
+import getSaltByEmail from "./db/getSaltByEmail.js";
 
 const app = express();
 
@@ -10,11 +12,41 @@ app.use(bodyParser.json());
 app.post("/api/signup", async (req, res) => {
     console.log(req.body);
 
-    const { email, password } = req.body;
-    const object = { email, password };
+    const { name, email, password, salt } = req.body;
+    const object = { name, email, password, salt };
     const result = await signup(dbConfig.url, dbConfig.databaseName, "users", object);
     console.log(result);
     res.send("Signup successful");
+});
+
+app.post("/api/login", async (req, res) => {
+    console.log(req.body);
+
+    const { email, password } = req.body;
+    const object = { email, password };
+    const result = await login(dbConfig.url, dbConfig.databaseName, "users", object);
+    console.log(result);
+    if (result) {
+        res.send("Login successful");
+    }
+    else{
+        res.send("Login unsuccessful");
+    }
+});
+
+app.post("/api/getSaltByEmail", async (req, res) => {
+    console.log(req.body);
+
+    const { email } = req.body;
+    const object = { email };
+    const result = await getSaltByEmail(dbConfig.url, dbConfig.databaseName, "users", object);
+    console.log(result);
+    if (result) {
+        res.send(result);
+    }
+    else{
+        res.send("Login unsuccessful");
+    }
 });
 
 app.listen(3000, () => {
